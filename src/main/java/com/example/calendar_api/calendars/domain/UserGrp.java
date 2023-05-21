53047467789a3e745f666f4e34c4e09d9c7d9888
KwonChoi <1222ck@naver.com>
2023-05-21 14:27:09 +0900

    다이어리 그룹 생성 1

---
+++ src/main/java/com/example/calendar_api/calendars/domain/UserGrp.java
@@ -0,0 +1,14 @@
+package com.example.calendar_api.calendars.domain;
+
+import javax.persistence.Column;
+
+public class UserGrp {
+    @Column(name = "GRP_ID", nullable = false)
+    private Integer grpId;
+
+    @Column(name="MEMBERS_IDX", length = 100, nullable = false)
+    private String membersId; // 제목
+
+    @Column(name="GRADE", length = 255, nullable = false)
+    private String grade; // 내용
+}
